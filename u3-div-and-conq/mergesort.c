acaa5b0df209d0950015d87170af6f4ebc7fde43
Shreyas S <shreyas14902@gmail.com>
2022-03-18 06:34:12 +0530

    u3

--- u3-div-and-conq/mergesort.c
+++ u3-div-and-conq/mergesort.c
@@ -1,85 +1,75 @@
 // mergesort program
 #include<stdio.h>
 #include<stdlib.h>
-
+void inp_arr(int *,int);
+void merge(int *,int,int,int);
+void mergesort(int *,int,int);
 int main()
 {
-    int i,j,k,n,*a,*b,*c;
+    int i,n,*a;
     printf("Enter the number of elements\n");
     scanf("%d",&n);
-    a=(int*)malloc(n*sizeof(int));
-    b=(int*)malloc(n*sizeof(int));
-    c=(int*)malloc(n*sizeof(int));
+    a=(int *)malloc(n*sizeof(int));
     printf("Enter the elements\n");
-    input_array(a,n);
-    mergesort(a,b,c,0,n-1);
+    inp_arr(a,n);
+    mergesort(a,0,n-1);
     printf("The sorted array is\n");
-    output_array(a,n);
+    for(i=0;i<n;i++)
+        printf("%d\n",a[i]);
     return 0;
 }
-void input_array(int *a, int n)
+void inp_arr(int *a,int n)
 {
     int i;
     for(i=0;i<n;i++)
-    {
         scanf("%d",&a[i]);
-    }
 }
-void output_array(int *a, int n)
-{
-    int i;
-    for(i=0;i<n;i++)
-    {
-        printf("%d\n",a[i]);
-    }
-}
-void mergesort(int *a, int *b, int *c, int l, int r)
+void mergesort(int *a,int l,int r)
 {
     int m;
     if(l<r)
     {
         m=(l+r)/2;
-        mergesort(a,b,c,l,m);
-        mergesort(a,b,c,m+1,r);
-        merge(a,b,c,l,m,r);
+        mergesort(a,l,m);
+        mergesort(a,m+1,r);
+        merge(a,l,m,r);
     }
 }
-void merge(int *a, int *b, int *c, int l, int m, int r)
+void merge(int *a,int l,int m,int r)
 {
-    int i,j,k;
-    i=l;
-    j=m+1;
+    int i,j,k,n1,n2,*b;
+    n1=m-l+1;
+    n2=r-m;
+    b=(int *)malloc(n1*sizeof(int));
+    for(i=0;i<n1;i++)
+        b[i]=a[l+i];
+    i=0;
+    j=0;
     k=l;
-    while(i<=m && j<=r)
+    while(i<n1 && j<n2)
     {
-        if(a[i]<=a[j])
+        if(b[i]<=a[m+1+j])
         {
-            c[k]=a[i];
+            a[k]=b[i];
             i++;
         }
         else
         {
-            c[k]=a[j];
+            a[k]=a[m+1+j];
             j++;
         }
         k++;
     }
-    while(i<=m)
+    while(i<n1)
     {
-        c[k]=a[i];
+        a[k]=b[i];
         i++;
         k++;
     }
-    while(j<=r)
+    while(j<n2)
     {
-        c[k]=a[j];
+        a[k]=a[m+1+j];
         j++;
         k++;
     }
-    for(i=l;i<=r;i++)
-    {
-        a[i]=c[i];
-    }
 }
-
-
